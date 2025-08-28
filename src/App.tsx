a64e52fe31aa96f0386227a35e71fd6d12e4f935
vipinsaini1989 <vipinsaini1989@gmail.com>
2025-08-29 00:11:56 +0530

    Init project with tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
